4afa52c73f26032fc4207b437973c8500c1967ec
izumi23 <57768307+izumi23@users.noreply.github.com>
2022-04-07 12:01:41 +0200

    Update test_fourier_bases.py

--- test_fourier_bases.py
+++ test_fourier_bases.py
@@ -1,24 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pygsp
 from pygsp import graphs, filters, plotting
 
 plt.ion()
 plotting.BACKEND = 'matplotlib'
 plt.rcParams['figure.figsize'] = (12, 8)
+plt.rcParams['figure.autolayout'] = True
 
 
 def show_fourier_basis(G):
+    plt.close('all')
+    nb_eig = 4
     G.compute_fourier_basis()
 
-    fig, axes = plt.subplots(2, 2)
-    axes_flat = axes.flatten()
-    for i, ax in enumerate(axes_flat):
-        if i == len(axes_flat)-1:
+    fig = plt.figure()
+    gs = plt.GridSpec(3, 2, height_ratios=[4, 4, 1])
+
+    for i in range(nb_eig):
+        ax = fig.add_subplot(gs[i])
+        if i == nb_eig-1:
             i = G.N - 2
         G.plot_signal(G.U[:, i+1], vertex_size=30, ax=ax)
         _ = ax.set_title('λ_{} = {:.3f}'.format(i+2, G.e[i+1]))
         ax.set_axis_off()
 
-    fig.tight_layout()
+    gs = plt.GridSpec(3, 1, height_ratios=[4, 4, 1])
+    ax = fig.add_subplot(gs[-1])
+    ax.plot(G.e, np.zeros_like(G.e), linestyle='None', marker='.', markersize=1)
 
-show_fourier_basis(graphs.Logo())
+
+## Tests
+
+#show_fourier_basis(graphs.Logo())
+#show_fourier_basis(graphs.Ring(50))
+
+G = graphs.FullConnected(10)
+G.set_coordinates()
+show_fourier_basis(G)
+
+show_fourier_basis(graphs.DavidSensorNet())
+
+
+
+
+
+
+
+
+
+
+
