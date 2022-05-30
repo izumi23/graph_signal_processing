3da35d531dbe2f7d5ebeb8f2a6c84613ec75b783
izumi23 <57768307+izumi23@users.noreply.github.com>
2022-05-30 16:11:04 +0200

    Filtrage rationnel (marche pas encore)

---
+++ rational_filter.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pygsp
+from pygsp import graphs, filters, plotting
+
+from test_signal_decomposition import smoothness_and_gft, show_fourier_decomposition, compare_fourier_decomposition
+from prony2 import prony
+
+##
+
+def ratfilter(wc, P, Q, sep=1, wmax=None, plot=False):
+    if wmax is None:
+        wmax = 5*wc
+    vx = np.concatenate((np.linspace(0, wc-sep/2, 10), np.linspace(wc+sep/2, wmax, 10)))
+    vy = (vx <= wc) + 0.01
+    return prony(vx, vy, P, Q, plot)
+
+def polyval(p, L):
+    N = L.shape[0]
+    H = np.zeros((N, N))
+    for pv in p:
+        H = L @ H + pv*np.eye(N)
+    return H
+
+def apply_ratfilter(p, q, L, s):
+    return np.linalg.solve(polyval(p, L), polyval(q, L) @ s)
+
+def snr(s2, s):
+    return -10 * np.log10( np.mean((s2-s)**2) / np.mean(s**2) )
+
+def low_pass_filter(G, s, s1, wc=None, wmax=None, P=6, Q=4):
+    if wc is None:
+        G.estimate_lmax()
+        wc = G.lmax/2
+    p, q = ratfilter(wc, P, Q, wmax)
+    s2 = apply_ratfilter(p, q, G.L, s1)
+    suptitle = "SNR = {:.1f} --> {:.1f} dB".format(snr(s1, s), snr(s2, s))
+    h = lambda w: np.polyval(q, w) / np.polyval(p, w)
+    compare_fourier_decomposition([G, G, G], [s, s1, s2], suptitle=suptitle, h=h)
+
+
+## On vérifie que le polynôme est bon
+
+p, q = ratfilter(10, 6, 4, sep=2, wmax=21, plot=True)
+p, q = ratfilter(2, 6, 4, wmax=8.5, plot=True)
+
+## Exemple 1 : Logo
+
+G = graphs.Logo()
+G.compute_fourier_basis()
+s = np.sum(G.U[:, :G.N//3], axis=1)
+s1 = s + np.random.normal(0, 0.1*np.max(np.abs(s)), size=G.N)
+low_pass_filter(G, s, s1, wc=4.4, wmax=14)
+
+## Exemple 2 : Bretagne
+
+H = np.loadtxt("data/GraphBretagneHybr.txt")
+coords = np.loadtxt("data/GraphCoords.txt")
+s = np.loadtxt("data/Temperature.txt")
+
+G = graphs.Graph(H)
+G.set_coordinates(coords)
+
+k = np.argmin([smoothness_and_gft(G, s[i])[0] for i in range(len(s))])
+
+s1 = s[k] + np.random.normal(0, 3, size=G.N)
+low_pass_filter(G, s[k], s1, wc=2, wmax=8.5)
+
