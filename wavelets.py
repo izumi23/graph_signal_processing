3a38a51984dbae7995911d97b9186cf15d23d227
izumi23 <57768307+izumi23@users.noreply.github.com>
2022-05-31 14:57:16 +0200

    Décomposition sur base d'ondelettes

--- wavelets.py
+++ wavelets.py
@@ -8,7 +8,9 @@ plt.show()
 
 ## Génération des filtres
 
-def g(x):
+gamma = 1.384
+
+def g0(x):
     if x < 1:
         return x**2
     elif x <= 2:
@@ -16,7 +18,7 @@ def g(x):
     else:
         return 4*x**-2
 
-gamma = 1.384
+g = np.vectorize(lambda x: g0(x)/gamma)
 
 def h(x, lmax):
     return gamma * np.exp(-( 10*x/(0.3*lmax) )**4)
@@ -24,18 +26,37 @@ def h(x, lmax):
 def frequencies(lmax, r):
     return np.geomspace(lmax/40, lmax/2, r)
 
-## Obtention d'un coefficient sur la base d'ondelettes
+## Calculs sur la base d'ondelettes
 
-def coef(G, s, node, freq_index, lmax=None):
+def coef(G, s, node, freq, lmax=None):
     if lmax is None:
         G.estimate_lmax()
         lmax = G.lmax
     d = np.zeros_like(s)
     d[node] = 1
-    if freq_index == 0:
-        return pygsp.filters.Filter(G, lambda x: h(x))
+    f = lambda x: (h(x, lmax) if freq == 0 else g(x/freq))
+    return pygsp.filters.Filter(G, f).filter(d) @ s
 
+def basis(G, r):
+    B = np.zeros((r+1, G.N, G.N))
+    G.estimate_lmax()
+    vfreq = np.concatenate(([0], frequencies(G.lmax, r)))
+    for i, freq in enumerate(vfreq):
+        f = lambda x: (h(x, lmax) if freq == 0 else g(x/freq))
+        for node in range(G.N):
+            d = np.zeros((G.N))
+            d[node] = 1
+            B[i, node] = pygsp.filters.Filter(G, f).filter(d)
+    return B
 
+def decomposition(G, s, r, B=None):
+    D = np.zeros((r+1, G.N))
+    if B is None:
+        B = basis(G, r)
+    for i in range(r+1):
+        for node in range(G.N):
+            D[i, node] = B[i, node] @ s
+    return D
 
 ##
 
@@ -49,6 +70,21 @@ ax.plot(l, np.array([h(x, lmax) for x in l]), label="passe-bas")
 for w0 in frequencies(lmax, r):
     ax.plot(l, np.array([g(x/w0) for x in l]), label="{:.2f}".format(w0))
 ax.legend()
+
+##
+
+G = graphs.DavidSensorNet()
+s = np.zeros((G.N))
+s[G.N//2] = 1
+D = decomposition(G, s, 5)
+plt.imshow(D)
+
+##
+
+G = graphs.DavidSensorNet()
+s = np.array([np.sin(G.coords[i,0]) for i in range(G.N)])
+D = decomposition(G, s, 5)
+plt.imshow(D)
 
 ##
 
